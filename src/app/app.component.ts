ea63477f71d911207cb85ef43625eabd9717acad
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-01-14 14:11:51 +0200

    created progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,5 +18,6 @@ export class AppComponent {
       isExpanded: false
     }
   ]
+  public progressValue = 25;
   title = 'angular-challenges';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AccordionComponent } from './components/accordion/accordion.component';
     ToolbarComponent,
     CardComponent,
     AccordionComponent,
+    ProgressBarComponent,
   ],
   imports: [
     BrowserModule,
