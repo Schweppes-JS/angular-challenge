eace607d8152525e85288806ff60a3f1abc17d72
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-01-26 13:00:47 +0200

    created  scroll to top button

---
+++ src/app/components/toolbar/top-of-page/top-of-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, HostListener } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+
+@Component({
+  selector: 'app-top-of-page',
+  templateUrl: './top-of-page.component.html',
+  styleUrls: ['./top-of-page.component.scss']
+})
+export class TopOfPageComponent {
+  public isShown: boolean = false;
+
+  @HostListener('window:scroll') onWindowScroll() {
+    const yCoordinate = this.ViewportScroller.getScrollPosition()[1];
+    this.isShown = yCoordinate > 400;
+    console.log(this.isShown)
+  }
+
+  constructor(private ViewportScroller: ViewportScroller) { }
+
+  public goToTop(): void {
+    this.ViewportScroller.scrollToPosition([0, 0]);
+  }
+
+}
