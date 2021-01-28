1eb9073a3701f018e56d53641d92ab1537a2fc9a
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-01-28 14:11:27 +0200

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { StarRatingsComponent } from './components/star-ratings/star-ratings.component';
-import { TopOfPageComponent } from './components/toolbar/top-of-page/top-of-page.component';
+import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
+import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -19,10 +22,12 @@ import { TopOfPageComponent } from './components/toolbar/top-of-page/top-of-page
     ProgressBarComponent,
     StarRatingsComponent,
     TopOfPageComponent,
+    ComponentDocumentationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/component-documentation/component-documentation.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { AccordionItem } from 'src/app/models/accordion-item.interface';
+
+@Component({
+  selector: 'app-component-documentation',
+  templateUrl: './component-documentation.component.html',
+  styleUrls: ['./component-documentation.component.scss']
+})
+export class ComponentDocumentationComponent {
+  public accordionItems: AccordionItem[] = [
+    {
+      title: 'Example 1',
+      content: 'Exampled Content 1',
+      isExpanded: false
+    },
+    {
+      title: 'Example 2',
+      content: 'Exampled Content 2',
+      isExpanded: false
+    }
+  ]
+  public progressValue = 25;
+  title = 'angular-challenges';
+}
