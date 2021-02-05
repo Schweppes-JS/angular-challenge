76634ff5803ca5a0d06248b5b89c806bfcc90990
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-02-05 15:39:20 +0200

    created credit card input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
@@ -12,6 +13,7 @@ import { TopOfPageComponent } from './components/top-of-page/top-of-page.compone
 import { ComponentDocumentationComponent } from './components/component-documentation/component-documentation.component';
 import { APP_ROUTES } from './app.routes';
 import { SharedModule } from './shared/shared.module';
+import { CreditCardInputComponent } from './components/credit-card-input/credit-card-input.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +23,14 @@ import { SharedModule } from './shared/shared.module';
     ProgressBarComponent,
     StarRatingsComponent,
     TopOfPageComponent,
-    ComponentDocumentationComponent
+    ComponentDocumentationComponent,
+    CreditCardInputComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [],
