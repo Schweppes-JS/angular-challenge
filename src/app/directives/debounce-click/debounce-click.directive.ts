713627b1e324f41fad100eff868936de976dfbe0
Schweppes-JS <shvetsdmytro90@ukr.net>
2021-02-22 15:41:32 +0200

    created debounce click directive

---
+++ src/app/directives/debounce-click/debounce-click.directive.ts
@@ -0,0 +1,32 @@
+import { HostListener, OnDestroy, EventEmitter } from '@angular/core';
+import { Directive, OnInit, Output } from '@angular/core';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime'
+
+@Directive({
+  selector: '[appDebounceClick]'
+})
+export class DebounceClickDirective implements OnInit, OnDestroy {
+  @Output() public debounceClick = new EventEmitter();
+  public clicks = new Subject();
+  public subscription!: Subscription;
+
+  public ngOnInit(): void {
+    const debounceTimeInMs = 500;
+
+    this.subscription = this.clicks.pipe(
+      debounceTime(debounceTimeInMs)
+    ).subscribe((value: any) => this.debounceClick.emit(value));
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  @HostListener('click', ['$event'])
+  public clickEvent(event: any): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.clicks.next();
+  }
+}
--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { DIRECTIVES_ROUTES } from './directives.routes';
 import { DirectiveDocumentationComponent } from './directive-documentation/directive-documentation.component';
 import { SharedModule } from '../shared/shared.module';
+import { DebounceClickDirective } from './debounce-click/debounce-click.directive';
 
 @NgModule({
-  declarations: [DirectiveDocumentationComponent],
+  declarations: [DirectiveDocumentationComponent, DebounceClickDirective],
   imports: [
     CommonModule,
     SharedModule,
